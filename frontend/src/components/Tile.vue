9e06509d92d8aa0e313531d991bcd7812a6eb484
Mikołaj <mikolajwojcik011@gmail.com>
2024-03-12 17:24:30 +0100

    tile fix

---
+++ frontend/src/components/Tile.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="tile"> <img src="https://1.bp.blogspot.com/-R-ab3bJBv6o/YRoctUuVKiI/AAAAAAAAG8A/eNHL8NAhJ6gRZBp3eW-KMzGOdrUsaFVrQCLcBGAsYHQ/s16000/youtubethumbnaildownloader.png" alt="Thumbnail"><h3 class="roboto-bold">Normalny tekst pzdr jak yt blyblybly</h3></div>
+
+</template>
+
+<script>
+ export default {
+    name: "Tile",
+    components: {},
+}
+</script>
+
+<style>
+  .tile {
+        box-sizing: border-box;
+        max-width: 344px;
+        margin-bottom: 2.5rem;
+    }
+
+    .tile img {
+        max-width: 344px;
+        height: auto;
+        display: block;
+        margin-bottom: 1rem;
+        border-radius: 1rem;
+    }
+
+    .tile h3 {
+        font-size: 1.2rem;
+        margin: 0;
+        margin-left: 1rem;
+
+    }
+    
+    .roboto-bold {
+        font-family: "Roboto", sans-serif;
+        font-weight: 500;
+        font-style: normal;
+    }
+</style>
